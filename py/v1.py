77c82103e387bcfcf4b2565f688e877a2622e9a9
liaopengkai <pengkailiao@gmail.com>
2013-10-18 00:39:28 +0800

    create2013

--- py/v1.py
+++ py/v1.py
@@ -272,6 +272,64 @@ def save20xx_1(y, num):
 #    print js
 #    print js['year']
 
+def save20xx_2(y,num):
+    f = open('htmlRes/%d.htm' %y, 'r')
+    content = f.read()
+    
+    key1 = r'<font face="Terminal">([^ ]*)</font></td>'
+    result1 = re.findall(key1, content)
+    
+    key2 = '<td width="187" align="center" valign="bottom" bgcolor="#FFFFFF">([^\t]*gif">*)</td>'
+    result2 = re.findall(key2, content)
+    
+    key3 = 'align="center" valign="middle" bgcolor="#FFFFFF" height="19"><IMG src="49m/([0-9]*).gif"></td>'
+    result3 = re.findall(key3, content)
+    
+    #    print 'reuslt1 = \n', result1
+    #     print 'reuslt2 = \n', result2
+    #     print 'reuslt3 = \n', result3
+    
+    #     print '\nitem ='
+    #     for item in result2:
+    #         print item
+    #         print '----------------------------'
+    
+    
+    
+    print 'reuslt1 num = ', len(result1)
+    print 'reuslt2 num = ', len(result2)
+    print 'reuslt3 num = ', len(result3)
+    
+    arr = []
+    for i in range(num):
+        
+        k = '<IMG src="49m/([0-9]*).gif"'
+        r = re.findall(k, result2[i])
+        #        print result2[i]
+        #        print r
+        #        print '*'*10
+        
+        t = result1[i*2]
+        n = result1[i*2+1]
+        d = r
+        s = result3[i]
+        
+        obj = {'t':t,
+            'n':n,
+            'd':d,
+            's':s
+        }
+        
+        arr.append(obj)
+    result = {'year':y, 'data':arr}
+    j = json.dumps(result)
+    m = MTool()
+    m.save('%d.json'%y, j, False, 'json/')
+
+#    fi = open('json/2012.json', 'r').read()
+#    js = json.loads(fi)
+#    print js
+#    print js['year']
 
 
 if __name__ == '__main__':
@@ -284,12 +342,12 @@ if __name__ == '__main__':
 
 #    key2 = '#FFFFFF">\n([0-9A-Za-z ="/.<>  \n]*)\b</td>'
 
-
+    save20xx_2(2013,119)
 #    save2012()
 #    save2011()
 #    save20xx(2010, 152)
 #    save20xx(2009, 154)
-    save20xx_1(2008, 149)
+    # save20xx_1(2008, 149)
 
 
     
